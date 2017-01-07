bec3b38baa42856bdabae915db48f81b130550f5
Brad Traversy <me@bradtraversy>
2017-01-07 17:34:44 -0500

    Changed ios file

--- index.ios.js
+++ index.ios.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react';
-import {AppRegistry, Text, View} from 'react-native';
+import {AppRegistry, Text, View, Navigator} from 'react-native';
 
-import Component1 from './app/components/Component1/Component1';
+import Component5 from './app/components/Component5/Component5';
+import Component6 from './app/components/Component6/Component6';
 
 export default class myapp extends Component{
+  renderScene(route, navigator){
+    switch(route.id){
+      case 'component5':
+        return (<Component5 navigator={navigator} title="component5" />)
+      case 'component6':
+        return (<Component6 user={route.user} navigator={navigator} title="component6" />)
+    }
+  }
+
   render(){
     return(
-      <View>
-        <Component1 />
-      </View>
+      <Navigator 
+        initialRoute={{id: 'component5'}}
+        renderScene={this.renderScene}
+        configureScreen={(route, routeStack) => Navigator.SceneConfigs.FloatFromBottom}
+      />
     );
   }
 }
